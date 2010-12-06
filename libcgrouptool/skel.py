9368cb28fcb5486c64106435efefc7ffa711c11c
José de Paula Eufrásio Júnior <jose.junior@gmail.com>
2010-12-06 18:16:26 -0200

    More work on the Cgroup class.
    Signed-off-by: José de Paula Eufrásio Júnior <jose.junior@gmail.com>

--- libcgrouptool/skel.py
+++ libcgrouptool/skel.py
@@ -18,6 +18,8 @@
 # You should have received a copy of the GNU General Public License
 # along with libcgrouptools.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+
 class Cgroup(object):
     """ Cgroup object to reflect the on-disk structure.
 
@@ -30,27 +32,74 @@ class Cgroup(object):
     """
     
     children = []
+    siblings = None
     path = ''
+    fspath = ''
     procs = []
 
-    def __init__(self, parent, cgroupmount = None):
+    def __init__(self, name, parent, cgroupmount = None):
         if parent == self:
             raise Exception("Cgroup can't be child of itself")
+            return
 
         if parent is not None:
             parent.children.append(self)
+            self.parent = parent
+            self.siblings = self.parent.children
+            self.__create_group(name)   
         else:
             self.path = '/'
             self.fspath = cgroupmount
+    
+    def __create_group(self, name):
+        new_path = os.path.join(self.parent.fspath, name)
+        try:
+            os.mkdir(new_path)
+        except:
+            raise Exception("Can't create the cgroup directory")
+            return
+        self.fspath = path
+        self.path = self.parent.path.rstrip('/') + '/%s' % name
+        
+    def __remove_group(self, name):
+        if len(self.children) > 0:
+            raise Exception("Can't remove a group with children")
+            return
+        try:
+            os.rmdir(self.fspath)
+        except:
+            raise Exception("Can't remove the cgroup directory")
+            return
+        self.parent.children.remove(self) 
+
+    def __add_pid_to_cgroup(self, pid):
+        tsk_file_path = os.path.join(self.fspath, 'tasks')
+        tsk_file = open(tsk_file_path)
+        try:
+            tsk_file.write(str(pid))
+        except:
+            raise Exception("Can't write to cgroup's tasks file")
+
+    def __remove_pid_from_cgroup(pid):
+        """In truth, moves the pid to the cgroup's parent cgroup,
+           there's no way to remove it from the tasks file"""
+        tsk_file_path = os.path.join(self.parent.fspath, 'tasks')
+        tsk_file = open(tsk_file_path)
+        try:
+            tsk_file.write(str(pid))
+        except:
+            raise Exception("Cant't write to parent cgroup's tasks file") 
     
     def addproc(self, pid):
         if pid in self.procs:
             raise Exception("pid already on this cgroup")
         else:
             self.procs.append(pid)
+            self.__add_pid_to_cgroup(pid)
 
     def removeproc(self,pid):
         if pid not in self.prcs:
             raise Exception("pid not on this cgroup")
         else:
             self.procs.remove(pid)
+            self.__remove_pid_from_cgroup(pid)
